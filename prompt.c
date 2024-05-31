0e765dc4bee698149437641e3978d9ce290ffdfd
EliezerSunny <adetunjieliazer@gmail.com>
2024-05-31 15:06:29 -0700

    modified

--- prompt.c
+++ prompt.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "shell.h"
 
 /**
  * prompt - call prompt from another function (prompt)
--- shell.c
+++ shell.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "shell.h"
 
 /**
  * main - func with infinite loop
